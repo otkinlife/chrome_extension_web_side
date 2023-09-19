9e2c050d2acc53e18890544fa5cef5d413ede458
kcjia <otkinlife@163.com>
2023-09-19 10:02:23 +0800

    0.0.1

--- sidepanel/sidepanel.js
+++ sidepanel/sidepanel.js
@@ -1,43 +1,8 @@
-const urlInput = document.getElementById('url-input');
-const visitBtn = document.getElementById('visit-btn');
 const iframeContainer = document.getElementById('underside-iframe-container');
 const iframe = document.getElementById('underside-iframe-container');
-const firstTimeGuide = document.getElementById('first-time-guide');
-
-function checkFirstTime() {
-  const isFirstTime = localStorage.getItem('isFirstTime');
-  if (!isFirstTime) {
-    firstTimeGuide.style.display = 'block';
-    localStorage.setItem('isFirstTime', 'false');
-  }
-}
-
-function openUrlInIframe(url) {
-  iframeContainer.src = url;
-}
-
-visitBtn.addEventListener('click', () => {
-  let url = urlInput.value;
-  url = ensureHttps(url);
-  openUrlInIframe(url);
-  firstTimeGuide.style.display = 'none';
-});
-
-urlInput.addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') {
-    let url = urlInput.value;
-    url = ensureHttps(url);
-    openUrlInIframe(url);
-    firstTimeGuide.style.display = 'none';
-  }
-});
-
-function ensureHttps(url) {
-  if (!url.startsWith('http://')) {
-    return 'http://' + url;
-  }
-  return url;
-}
+const urlInput = document.getElementById('url');
+const titleInput = document.getElementById('title');
+const confirmButton = document.getElementById('confirm');
 
 function sendEventToIframe(eventName, eventArgs) {
   console.debug('sendEventToIframe', eventName, JSON.stringify(eventArgs));
@@ -84,5 +49,78 @@ function buildActiveTabInfo(tab) {
   };
 }
 
-// 新添加的代码
-checkFirstTime();
+let tabCounter = 1;
+
+function createTab(url, title) {
+  tabCounter++;
+  const newTab = document.createElement('div');
+  newTab.className = 'tab';
+  newTab.dataset.id = tabCounter;
+  newTab.innerHTML = `<span class="tab-title">${title}</span><img class="close-tab" src="../close.png" alt="Close Tab" />`;
+  document.querySelector('.tabs-bar').insertBefore(newTab, document.querySelector('.add-tab'));
+
+  const newIframeContainer = document.createElement('div');
+  newIframeContainer.className = 'iframe-container';
+  newIframeContainer.dataset.id = tabCounter;
+  newIframeContainer.innerHTML = `<iframe frameborder="0" allow="clipboard-write" src="${url}"></iframe>`;
+  document.querySelector('.iframes').appendChild(newIframeContainer);
+}
+
+function switchTab(tabId) {
+  document.querySelectorAll('.tab, .iframe-container').forEach(element => {
+    if (element.dataset.id === tabId) {
+      element.classList.add('active');
+    } else {
+      element.classList.remove('active');
+    }
+  });
+}
+
+document.querySelector('.add-tab').addEventListener('click', function () {
+  const dialog = document.getElementById('dialog');
+  dialog.style.display = 'block';
+});
+
+// 检查输入框是否为空，如果为空则禁用确认按钮
+function checkInputs() {
+  if (!urlInput.value || !titleInput.value) {
+    confirmButton.disabled = true;
+  } else {
+    confirmButton.disabled = false;
+  }
+}
+
+// 添加 input 事件监听器
+urlInput.addEventListener('input', checkInputs);
+titleInput.addEventListener('input', checkInputs);
+
+// 确认按钮的点击事件
+confirmButton.addEventListener('click', function () {
+  if (!confirmButton.disabled) {
+    createTab(urlInput.value, titleInput.value);
+    switchTab(tabCounter.toString());
+    // 清空输入框并隐藏对话框
+    urlInput.value = '';
+    titleInput.value = '';
+    document.getElementById('dialog').style.display = 'none';
+  }
+});
+
+// 取消按钮的点击事件
+document.getElementById('cancel').addEventListener('click', function () {
+  // 清空输入框并隐藏对话框
+  urlInput.value = '';
+  titleInput.value = '';
+  document.getElementById('dialog').style.display = 'none';
+});
+
+
+document.addEventListener('click', function (e) {
+  if (e.target.className === 'close-tab') {
+    const tabId = e.target.parentNode.dataset.id;
+    document.querySelector(`.tab[data-id="${tabId}"]`).remove();
+    document.querySelector(`.iframe-container[data-id="${tabId}"]`).remove();
+  } else if (e.target.classList.contains('tab')) {
+    switchTab(e.target.dataset.id);
+  }
+});
